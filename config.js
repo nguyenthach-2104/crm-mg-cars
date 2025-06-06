5a127a3bdd50335cd97ba3edaa154465632fa132
Thach <mkt.mgbinhduong@gmail.com>
2025-06-06 21:17:31 +0700

    Tích hợp OAuth 2.0 để đọc/ghi Google Sheets

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    clientId: '478864658701-j9h1tsme9a8tq9aolm83ql9cof9ramb1.apps.googleusercontent.com', // Từ JSON
-    apiKey: 'AIzaSyBE5nHSYaasu6RLrdE5QqnDQaD4Q1XAp9I', // API Key cũ
+    clientId: '478864658701-j9h1tsme9a8tq9aolm83ql9cof9ramb1.apps.googleusercontent.com', // Từ JSON OAuth
+    apiKey: 'AIzaSyBE5nHSYaasu6RLrdE5QqnDQaD4Q1XAp9I',
     spreadsheetId: '1C4J3KBvE-wxSFBJywbV0W5IkFaXnI-aArNazLr5napE',
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     scope: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/userinfo.email'
